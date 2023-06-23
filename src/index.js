de039b3a4c54ab6a5581f395c3d3a3ec2ec85cfc
Lysenko <lysenkoden@gmail.com>
2023-06-23 06:59:59 +0200

    initial new project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const ulElement = document.querySelector(".posts-list");
+// const arr = fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((response) => response.json())
+//   .then((posts) => {
+//     return posts;
+//   });
+
+// const myPosts = async () => {
+//   try {
+//     const posts2 = await arr;
+//     let result = posts2.map(function (elements) {
+//       return elements;
+//     });
+//     result.forEach((elem) => {
+//       ulElement.insertAdjacentHTML(
+//         "afterbegin",
+//         `<li class="posts-list-item"><span class="post-index">${
+//           elem.id
+//         }</span><h4 class="post-title">${elem.title}</h4><p class="post-text">${
+//           elem.body
+//         }</p><a href="#" class="post-link">${"Read more"}</a></li>`
+//       );
+//     });
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// };
+// myPosts();
