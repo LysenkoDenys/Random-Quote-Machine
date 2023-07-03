65bdc893b903dc76d4c80ada700297221b2e1478
Lysenko <lysenkoden@gmail.com>
2023-07-03 08:24:02 +0200

    config tweet link and fix tab icon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import Panel from "./components/Panel";
+import generateRandomNumberForColor from "./utils/generateRandomNumberForColor";
 
 function App() {
+  const randColor = `rgba(${generateRandomNumberForColor()},${generateRandomNumberForColor()},${generateRandomNumberForColor()},1)`;
+  document.body.style.backgroundColor = randColor;
   return (
-    <div className="bg-[#288a69] mx-auto max-w-8xl px-10 py-10 sm:px-6 lg:px-8">
+    <div className="mx-auto my-auto max-w-8xl px-10 py-10 font-sans">
       <Panel initialClassesList="" />
+      <p className="text-center text-white mt-3">
+        by{" "}
+        <a
+          href="https://www.linkedin.com/in/lysenko-denys/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Lysenko Denys
+        </a>
+      </p>
     </div>
   );
 }
--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { FaTwitter } from "react-icons/fa";
+import { FaTwitter, FaQuoteLeft } from "react-icons/fa";
 import generateRandomNum from "../utils/generateRandomNum";
 import generateRandomNumberForColor from "../utils/generateRandomNumberForColor";
 
@@ -8,10 +8,21 @@ const Panel = (props) => {
   const [randomNum, setRandomNum] = useState(null);
   const [classesList, setClassesList] = useState(props.initialClassesList);
   const maxNum = data ? data.quotes.length : "there is no data";
+  const randColor = `text-[rgba(${generateRandomNumberForColor()},${generateRandomNumberForColor()},${generateRandomNumberForColor()},1)]`;
+  const randNumber = generateRandomNum(maxNum);
+  const url = data
+    ? "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
+      '"' +
+      data.quotes[randNumber].quote +
+      '" - ' +
+      data.quotes[randNumber].author
+    : "#";
+  const textField = `"${randColor} font-medium text-[1.75em] mb-4"`;
 
   const onButtonClick = () => {
     setData(data);
-    setClassesList("quote-class");
+    setClassesList(randColor);
+    console.log(textField); //
     setRandomNum(generateRandomNum(maxNum));
   };
 
@@ -31,40 +42,43 @@ const Panel = (props) => {
   }, []);
 
   return (
-    <div
-      id="quote-box"
-      className="bg-[#d6d7da] rounded-[5px] m-10 text-[18px] shadow-[0_35px_40px_-15px_rgba(0,0,0,0.3)] hover:bg-[#f3f4f5] hover:shadow-[2px_2px_2px_rgba(0,0,0,0.5)]
-    mx-auto max-w-4xl px-4 py-6 sm:px-6 lg:px-2"
-    >
-      {data && (
-        <div>
-          <p className={classesList} id="text">
-            "{data.quotes[generateRandomNum(maxNum)].quote}"
-          </p>
-          {/* got to move this span to the right */}
-          <span id="author" className="text-[1em] text-right">
-            - {data.quotes[generateRandomNum(maxNum)].author}
-          </span>
-        </div>
-      )}
-      <div className="flex flex-row justify-between mt-4 px-4">
-        <div>
-          <button className="inline-flex items-center rounded-md px-2 py-2 text-xl font-medium text-[rgba(255,255,255,1)] bg-[rgba(1,1,1,0.5)] ring-1 ring-inset ring-gray-500/10 hover:bg-[rgba(1,1,1,0.3)]">
+    <div id="wrapper">
+      <div
+        id="quote-box"
+        className="bg-[#d6d7da] rounded-[5px] m-3 text-[18px] shadow-[0_35px_40px_-15px_rgba(0,0,0,0.3)] hover:bg-[#f3f4f5] hover:shadow-[2px_2px_2px_rgba(0,0,0,0.5)]
+    mx-auto my-auto max-w-xl px-6 py-6"
+      >
+        {data && (
+          <div>
+            <p className={textField} id="text">
+              <FaQuoteLeft className="text-[25px] content-['\f10d']" />
+              {data.quotes[randNumber].quote}
+            </p>
+            <p
+              id="author"
+              className="text-[1em] float-right text-[rgba(52,34,36,1)]"
+            >
+              - {data.quotes[randNumber].author}
+            </p>
+          </div>
+        )}
+        <br></br>
+        <div className="flex flex-row justify-between mt-8">
+          <button className="inline-flex items-center rounded-[3px] px-2 py-2 text-xl font-medium text-[rgba(255,255,255,1)] bg-[rgba(1,1,1,0.5)] ring-1 ring-inset ring-gray-500/10 hover:bg-[rgba(1,1,1,0.3)]">
             <a
               id="tweet-quote"
               title="Tweet this quote!"
-              target="_top"
-              href="https://twitter.com/intent/tweet?hashtags=quotes"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={url}
             >
               <FaTwitter />
             </a>
           </button>
-        </div>
-        <div>
           <button
             onClick={onButtonClick}
             id="new-quote"
-            className="inline-flex items-center rounded-md px-2 py-1 text-xl font-medium text-[rgba(255,255,255,1)] bg-[rgba(1,1,1,0.5)] ring-1 ring-inset ring-gray-500/10 hover:bg-[rgba(1,1,1,0.3)]"
+            className="inline-flex items-center rounded-[3px] px-2 py-1 text-xl font-medium text-[rgba(255,255,255,1)] bg-[rgba(1,1,1,0.5)] ring-1 ring-inset ring-gray-500/10 hover:bg-[rgba(1,1,1,0.3)]"
           >
             New quote
           </button>
@@ -75,7 +89,3 @@ const Panel = (props) => {
 };
 
 export default Panel;
-
-{
-  /* <a id="tweet-quote" title="Tweet this quote!" target="_top" href="https://twitter.com/intent/tweet?hashtags=quotes&amp;related=freecodecamp&amp;text=%22The%20mind%20is%20everything.%20What%20you%20think%20you%20become.%22%20Buddha" ></a> */
-}
